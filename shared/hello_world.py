3a9c7dbcfd83e94c86ffa15e9ef83d0a37c7e085
Cristian OV <cristianov19@gmail.com>
2023-09-14 22:10:56 -0600

    Update hello_world.py
    toast implementation for main file

--- shared/hello_world.py
+++ shared/hello_world.py
@@ -1,2 +1,19 @@
 print("\n Hello World, Github People \n")
 
+# i wanna show a diferent way to show the [hello World]
+# pip install Windows-Toasts
+
+# We import WindowsToaster and a toast format we want
+from windows_toasts import Toast, WindowsToaster
+# Prepare the toaster for bread (or your notification)
+toaster = WindowsToaster('Notification service')
+# Initialise the toast
+newToast = Toast()
+# Set the body of the notification
+newToast.text_fields = ['Hello World... Github People! \nClick to continue...']
+
+# Activate actions
+newToast.on_activated = lambda _: print('Toast clicked!')
+
+# And display it!
+toaster.show_toast(newToast)
