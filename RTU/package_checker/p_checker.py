e4ab373a3737d913db177db2e5f7a3914b67f565
Cristian OV <cristianov19@gmail.com>
2023-09-16 21:50:46 -0600

    deploy file
    making a file to install all the necessary  and be used as executable.

--- RTU/package_checker/p_checker.py
+++ RTU/package_checker/p_checker.py
@@ -1,4 +1,6 @@
 import sys
 import subprocess
 # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', ''])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'webdriver-manager'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'selenium'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'beautifulsoup4 '])
---
+++ deploy.bat
@@ -0,0 +1,8 @@
+
+# Verificar el tipo de sistema 64 o 32 y si el sistema es windows y no linux
+# Puede implementarse para linux despues
+ 
+pip install selenium
+pip install webdriver-manager
+pip install beautifulsoup4 
+pip install pandas
